c221551f276ffe56f531ddd475c270dbc8b17d78
Saket kumar <46194436+saketkum11@users.noreply.github.com>
2021-11-19 13:24:56 +0530

    update till profite

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   var [stockQuantiy, setStockQuantiy] = useState(0);
   var [purcased, setPurcased] = useState(0);
   var [current, setCurrent] = useState(0);
+  var [display, setDisplay] = useState("");
   function stockQuantityHandler(event) {
     setStockQuantiy(event.target.value);
   }
@@ -15,7 +16,24 @@ export default function App() {
   function currentPrice(event) {
     setCurrent(event.target.value);
   }
-  function ClickHandler() {}
+  function calculateProfite(stockQuantiy, purcased, current) {
+    if (current > purcased) {
+      var profite = (current - purcased) * stockQuantiy;
+    }
+    return profite;
+  }
+  function ClickHandler() {
+    if (stockQuantiy !== "" && purcased !== "" && current !== "") {
+      if (stockQuantiy > 0 && purcased > 0 && current > 0) {
+        var profite = calculateProfite(stockQuantiy, purcased, current);
+        setDisplay(profite);
+      } else {
+        setDisplay("enter positive value");
+      }
+    } else {
+      setDisplay("Enter some value ");
+    }
+  }
   return (
     <div className="App">
       <h1>Saket kumar</h1>
@@ -29,6 +47,7 @@ export default function App() {
       <h1>{stockQuantiy}</h1>
       <h1>{purcased}</h1>
       <h1>{current}</h1>
+      <h1>{display}</h1>
     </div>
   );
 }
