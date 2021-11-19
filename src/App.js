dd756565537dd0ccf07c78ec71e0859941a24715
Saket kumar <46194436+saketkum11@users.noreply.github.com>
2021-11-19 14:35:47 +0530

    update till jsx display

--- src/App.js
+++ src/App.js
@@ -6,32 +6,49 @@ export default function App() {
   var [purcased, setPurcased] = useState(0);
   var [current, setCurrent] = useState(0);
   var [display, setDisplay] = useState("");
-  function stockQuantityHandler(event) {
-    setStockQuantiy(event.target.value);
+  var [message, setMessage] = useState("");
+
+  function stockQuantityHandler(e) {
+    var value = Number(e.target.value);
+    console.log(value);
+
+    setStockQuantiy(value);
   }
 
   function purcasePrice(event) {
-    setPurcased(event.target.value);
+    var value = Number(event.target.value);
+    console.log(value);
+    setPurcased(value);
   }
-  function currentPrice(event) {
-    setCurrent(event.target.value);
+  function currentPrice(item) {
+    var value = Number(item.target.value);
+    console.log(value);
+    setCurrent(value);
   }
-  function calculateProfite(stockQuantiy, purcased, current) {
-    if (current > purcased) {
-      var profite = (current - purcased) * stockQuantiy;
-    }
-    return profite;
-  }
-  function ClickHandler() {
+
+  // function calculateProfite(stockQuantiy, purcased, current) {
+  //  var gained = (current - purcased) * stockQuantiy;
+  //  console.log(gained);
+  //   return gained;
+  // }
+  function clickHandler() {
     if (stockQuantiy !== "" && purcased !== "" && current !== "") {
       if (stockQuantiy > 0 && purcased > 0 && current > 0) {
-        var profite = calculateProfite(stockQuantiy, purcased, current);
-        setDisplay(profite);
+        if (current > purcased) {
+          var profite = (current - purcased) * stockQuantiy;
+          var percentage = (profite / (purcased * stockQuantiy)) * 100;
+          setDisplay(profite);
+          setMessage("Gained");
+        } else if (purcased > current) {
+          var loss = (purcased - current) * stockQuantiy;
+          setDisplay(loss);
+          setMessage("Lossed");
+        }
       } else {
         setDisplay("enter positive value");
       }
     } else {
-      setDisplay("Enter some value ");
+      setDisplay("Enter some value");
     }
   }
   return (
@@ -39,15 +56,17 @@ export default function App() {
       <h1>Saket kumar</h1>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <label>Stock Quantity</label>
       <input type="number" onChange={stockQuantityHandler} />
+      <label>Purchased Price</label>
       <input type="number" onChange={purcasePrice} />
+      <label>Current Price</label>
       <input type="number" onChange={currentPrice} />
-      <button onClick={ClickHandler}>Check</button>
+      <button onClick={clickHandler}>Check</button>
 
-      <h1>{stockQuantiy}</h1>
-      <h1>{purcased}</h1>
-      <h1>{current}</h1>
-      <h1>{display}</h1>
+      <h1 style={{}}>
+        {message} {display} {} {}
+      </h1>
     </div>
   );
 }
