2fe42bfa4b8e1e8976315b61962c5c0e5abd0e2d
Saket kumar <46194436+saketkum11@users.noreply.github.com>
2021-11-19 16:58:35 +0530

    update in js and css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
   var [current, setCurrent] = useState(0);
   var [display, setDisplay] = useState("");
   var [message, setMessage] = useState("");
+  var [status, setStatus] = useState("");
 
   function stockQuantityHandler(e) {
     var value = Number(e.target.value);
@@ -26,36 +27,39 @@ export default function App() {
     setCurrent(value);
   }
 
-  // function calculateProfite(stockQuantiy, purcased, current) {
-  //  var gained = (current - purcased) * stockQuantiy;
-  //  console.log(gained);
-  //   return gained;
-  // }
   function clickHandler() {
     if (stockQuantiy !== "" && purcased !== "" && current !== "") {
       if (stockQuantiy > 0 && purcased > 0 && current > 0) {
         if (current > purcased) {
           var profite = (current - purcased) * stockQuantiy;
           var percentage = (profite / (purcased * stockQuantiy)) * 100;
+          setStatus(percentage);
           setDisplay(profite);
           setMessage("Gained");
         } else if (purcased > current) {
           var loss = (purcased - current) * stockQuantiy;
+          var lossed = (loss / (purcased * stockQuantiy)) * 100;
+
           setDisplay(loss);
           setMessage("Lossed");
+          setStatus(lossed);
         }
       } else {
         setDisplay("enter positive value");
+
+        setMessage("");
+        setStatus("");
       }
     } else {
       setDisplay("Enter some value");
+      setMessage("");
+      setStatus("");
     }
   }
   return (
     <div className="App">
       <h1>Saket kumar</h1>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+
       <label>Stock Quantity</label>
       <input type="number" onChange={stockQuantityHandler} />
       <label>Purchased Price</label>
@@ -64,8 +68,13 @@ export default function App() {
       <input type="number" onChange={currentPrice} />
       <button onClick={clickHandler}>Check</button>
 
-      <h1 style={{}}>
-        {message} {display} {} {}
+      <h1
+        style={{
+          backgroundColor: "white",
+          color: message === "Gained" ? "green" : "red"
+        }}
+      >
+        {message} {display} {status}
       </h1>
     </div>
   );
