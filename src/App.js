c793dde822c0d21fc2ea17c1cfcd4e59159b9df4
Saket kumar <46194436+saketkum11@users.noreply.github.com>
2021-11-19 12:51:03 +0530

    update till clickhandler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  function stockQuantityHandler(event) {}
+
+  function purcasePrice(event) {}
+  function currentPrice(event) {}
+  function ClickHandler() {}
   return (
     <div className="App">
       <h1>Saket kumar</h1>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <input type="number" onChange={stockQuantityHandler} />
+      <input type="number" onChange={purcasePrice} />
+      <input type="number" onChange={currentPrice} />
+      <button onClick={ClickHandler}>Check</button>
     </div>
   );
 }
